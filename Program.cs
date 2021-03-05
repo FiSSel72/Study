5a31f01c72182bfae144f587ee33a4271ac489b9
FiSSel72 <65276837+FiSSel72@users.noreply.github.com>
2021-03-05 18:40:23 +0200

    Database first model
    Database first model that uses SQL DB created in ConsoleApp37

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp37_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (firstdbContext db = new firstdbContext())
+            {
+                var users = db.Users.ToList();                  
+                Console.WriteLine("Список объектов: ");
+                foreach (User u in users)                       
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ firstdbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ConsoleApp37_2
+{
+    public partial class firstdbContext : DbContext
+    {
+        public firstdbContext()
+        {
+        }
+
+        public firstdbContext(DbContextOptions<firstdbContext> options): base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=firstdb;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+
+}
