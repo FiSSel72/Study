f24c3e9dfd89c431e02836d8dd27581a1468478f
FiSSel72 <65276837+FiSSel72@users.noreply.github.com>
2021-03-05 18:47:58 +0200

    CRUD methods
    CreateReadUpdateDelete methods

--- Program.cs
+++ Program.cs
@@ -1,23 +1,91 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
-namespace ConsoleApp37_2
+namespace ConsoleApp37_3
 {
-    class Program
+    public class User
     {
-        static void Main(string[] args)
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
         {
-            using (firstdbContext db = new firstdbContext())
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=firstdb;Trusted_Connection=True;");
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Добавление
+            using (ApplicationContext db = new ApplicationContext())
             {
-                var users = db.Users.ToList();                  
-                Console.WriteLine("Список объектов: ");
-                foreach (User u in users)                       
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Alice", Age = 26 };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+            }
+
+            // Получение
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var users = db.Users.ToList();
+                Console.WriteLine("Данные после добавления:");
+                foreach (User u in users)
                 {
                     Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
                 }
             }
-            Console.ReadKey();
+
+            // Редактирование
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                User user = db.Users.FirstOrDefault();
+                if (user != null)
+                {
+                    user.Name = "Bob";
+                    user.Age = 44;
+                    db.SaveChanges();
+                }
+                Console.WriteLine("\nДанные после редактирования:");
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+
+            // Удаление
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                
+                User user = db.Users.FirstOrDefault();
+                if (user != null)
+                {
+                    
+                    db.Users.Remove(user);
+                    db.SaveChanges();
+                }            
+                Console.WriteLine("\nДанные после удаления:");
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+            Console.Read();
         }
     }
 }
-
