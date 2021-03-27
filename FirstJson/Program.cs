aaef440b9747edabb31dc954afc24f2e742e83b6
FiSSel <golovchenkoartem03@gmail.com>
2021-03-27 16:53:37 +0200

    Добавьте файлы проекта.

---
+++ FirstJson/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace FirstJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = File.Exists("user.json")? JsonConvert.DeserializeObject<User>(File.ReadAllText("user.json")): new User 
+            {
+                age = 17,
+                FirstName = "Artem",
+                LastName = "Golovchenko"
+                
+            };
+
+            user.age++;
+
+            File.WriteAllText("user.json", JsonConvert.SerializeObject(user));
+
+            Console.ReadKey();
+        }
+    }
+}
